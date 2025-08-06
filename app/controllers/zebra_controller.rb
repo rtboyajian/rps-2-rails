d8e43e677637416c433a780596624793a8d68ca9
rtboyajian <rtboyajian@gmail.com>
2025-08-06 20:48:34 +0000

    woot

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,49 @@
+class ZebraController < ApplicationController
+  
+  def rock
+    play("rock")
+  end
+
+  def paper
+    play("paper")
+  end
+
+  def scissors
+    play("scissors")
+  end
+
+  def rules
+    render({:template => "game_templates/rules"})
+  end
+
+private
+
+  # this will run for all three moves
+  def play(player_choice)
+    @player_move = player_choice
+    @computer_move = ["rock", "paper", "scissors"].sample
+    @outcome = determine_outcome(@player_move, @computer_move)
+
+    #renders e.g., app/views/game_templates/play_rock.html.erb
+
+    render({:template => "game_templates/play_#{player_choice}"})
+  end
+
+  # returns "win", "lose", or "tie"
+  def determine_outcome(player, computer)
+    wins = {
+      "rock" => "scissors",
+      "scissors" => "paper",
+      "paper" => "rock"
+    }
+
+
+    if player == computer
+      "tie"
+    elsif wins.fetch(player) == computer
+      "win"
+    else
+      "lose"
+    end 
+  end
+end
